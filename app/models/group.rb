1b8d5e666558f1d22ff8383496e9d65bf82bd5d3
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-29 14:18:48 +0200

    create validation, create roles, add devise

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :payments
+
+  validates :name, presence :true
+  validates :user_id, presence :true
+  validates :string, presence :true
 end
