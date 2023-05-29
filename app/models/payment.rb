0ae466f66cae185f975ee47b64450425e4e021ba
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-29 15:11:11 +0200

    payment validation setup

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,9 @@
 class Payment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :groups
+
+
+  validates :amount, presence: true
+  validates :user_id, presence: true
+  validates :group_id, presence: true
 end
