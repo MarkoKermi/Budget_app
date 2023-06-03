2b61c2ebb286b96c71b45f7e215799cfb6108577
Marko Kermiciev <kermicievmarko@gmail.com>
2023-06-03 20:12:01 +0200

    creating rspec test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,24 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  before(:each) do
-    @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
-                        password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
-    @group = Group.create(name: 'Test group', icon: 'http://example.com', author_id: @user.id)
+RSpec.describe Group, type: :model do
+  before :all do
+    @group =
+      Group.create(
+        name: 'test category',
+        icon: 'shopping_cart',
+        user_id: User.first
+      )
+    @payment = Payment.create(
+      name: 'weeky groceries',
+      amount: 15.50,
+      author_id: User.first.id
+    )
+
+    @payment_item = PaymentItem.create(
+      payment_id: Payment.first.id,
+      group_id: Group.first.id
+    )
   end
-  it 'is valid with valid attributes' do
-    expect(@group).to be_valid
+
+  describe 'check category validity' do
+    it 'is valid with valid attributes' do
+      expect(@group).to be_valid
+    end
   end
-  it 'is not valid without a name' do
-    @group.name = nil
-    expect(@group).to_not be_valid
+
+  describe 'check category invalidity' do
+    it 'is invalid with invalid name' do
+      @group.name = nil
+      expect(@group).to_not be_valid
+    end
+
+    it 'is invalid with invalid icon' do
+      @group.icon = nil
+      expect(@group).to_not be_valid
+    end
   end
-  it 'is not valid without an icon' do
-    @group.icon = nil
-    expect(@group).to_not be_valid
-  end
-  it 'is not valid without an author_id' do
-    @group.author_id = nil
-    expect(@group).to_not be_valid
+
+  describe '#total_amount' do
+    it 'return total amount for category' do
+      expect(@group.total_amount_by_group).to eql 0
+    end
   end
 end
+
+
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe payment, type: :model do
-  before(:each) do
-    @user = User.create(name: 'Test user', email: 'test444@gmail.com', password: '123456',
-                        password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
-    @group = Group.create(name: 'Test group', icon: 'http://example.com', author_id: @user.id)
-    @payment = payment.create(name: 'Test payment', amount: 100, author_id: @user.id)
+RSpec.describe Payment, type: :model do
+  subject do
+    Payment.new(
+      name: 'test spending',
+      amount: 10.50,
+      author_id: User.first
+    )
   end
-  it 'is valid with valid attributes' do
-    expect(@payment).to be_valid
-  end
-  it 'is not valid without a name' do
-    @payment.name = nil
-    expect(@payment).to_not be_valid
-  end
-  it 'is not valid without an amount' do
-    @payment.amount = nil
-    expect(@payment).to_not be_valid
-  end
-  it 'is not valid without an author_id' do
-    @payment.author_id = nil
-    expect(@payment).to_not be_valid
+
+  describe 'check spendign invalidity' do
+    it 'is invalid with invalid name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid with invalid amount' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
+    it 'is invalid with negative amount' do
+      subject.amount = -5.0
+      expect(subject).to_not be_valid
+    end
   end
 end
